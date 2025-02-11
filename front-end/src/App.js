9230ae605c3d50763d28196bbf9a24d3c37c2f8d
N <negar.sakhaei@yahoo.com>
2025-02-10 17:04:23 -0700

    prettier js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
+import "./App.css";
+import HeaderBar from "./headerbar.js";
+import Body from "./body.js";
+import Footer from "./footer.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<HeaderBar />
+			<Body />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- routes/api.js
+++ routes/api.js
@@ -1,38 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Tasks = require('../models/tasks');
+const Tasks = require("../models/tasks");
 
-router.get('/tasks', (req, res, next) => {
-    Tasks.find({}, ['action', 'status', 'type_of_day'])
-        .then((data) => res.json(data))
-        .catch(next);
+router.get("/tasks", (req, res, next) => {
+	Tasks.find({}, ["action", "status", "type_of_day"])
+		.then((data) => res.json(data))
+		.catch(next);
 });
 
-router.get('/bones', (req, res, next) => {
-    Tasks.find({type_of_day: 'bones'}, ['action', 'status', 'type_of_day'])
-        .then((data) => res.json(data))
-        .catch(next);
+router.get("/bones", (req, res, next) => {
+	Tasks.find({ type_of_day: "bones" }, ["action", "status", "type_of_day"])
+		.then((data) => res.json(data))
+		.catch(next);
 });
 
-router.get('/nobones', (req, res, next) => {
-    Tasks.find({type_of_day: 'no bones'}, ['action', 'status', 'type_of_day'])
-        .then((data) => res.json(data))
-        .catch(next);
+router.get("/nobones", (req, res, next) => {
+	Tasks.find({ type_of_day: "no bones" }, ["action", "status", "type_of_day"])
+		.then((data) => res.json(data))
+		.catch(next);
 });
 
-router.post('/tasks', (req, res, next) => {
-    if (req.body.action) {
-        Tasks.create(req.body)
-            .then((data) => res.json(data))
-            .catch(next);
-    } 
-    else {
-        res.json({
-            error: 'The input field is empty',
-            stuff: req.body,
-        });
-    }
+router.post("/tasks", (req, res, next) => {
+	if (req.body.action) {
+		Tasks.create(req.body)
+			.then((data) => res.json(data))
+			.catch(next);
+	} else {
+		res.json({
+			error: "The input field is empty",
+			stuff: req.body,
+		});
+	}
 });
 
 module.exports = router;
-  
